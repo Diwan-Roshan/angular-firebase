729c606b26d48f58598599eb4bf348e330f4a8bb
Diwan Roshan <roshand@yupptv.com>
2022-08-08 18:04:05 +0530

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,12 @@ import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { AngularFireModule } from '@angular/fire/compat';
-import { AngularFireAuthModule } from '@angular/fire/compat/auth';
-import { AngularFireStorageModule } from '@angular/fire/compat/storage';
-import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
-import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 import { environment } from '../environments/environment';
 import { SigninComponent } from './signin/signin.component';
 import { HeaderComponent } from './header/header.component';
 import { BannerComponent } from './banner/banner.component';
 import { CardsComponent } from './cards/cards.component';
+import { DetailsPageComponent } from './details-page/details-page.component';
 
 @NgModule({
   declarations: [
@@ -21,13 +17,13 @@ import { CardsComponent } from './cards/cards.component';
     SigninComponent,
     HeaderComponent,
     BannerComponent,
-    CardsComponent
+    CardsComponent,
+    DetailsPageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     RouterModule,
-    AngularFireModule.initializeApp(environment.firebaseConfig),
   ],
   
   providers: [],
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { generate } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  public object = [{'a':{'id':'1','name':'dr'},'b':{'id':'2','name':'drs'},'c':{'id':'3','name':'drr'}}]
   constructor() { }
 
+  
   ngOnInit(): void {
+    var output = [];
+    for (var i=0; i < this.object.length ; ++i)
+    console.log(this.object)
+        output.push(this.object[i]);
+        console.log(output)
+    // return output;
+    // let bb = Object.keys(this.object.a)
+    // console.log(bb[Math.floor(Math.random()*bb.length)])
   }
+
+  openNav():any{
+    // document.getElementById("mySidebar").style.width = "200px";
+    // this.base_right = true;
+    document.body.classList.add("modal-visible");
+  }
+
+  closeNav():any {
+    // document.getElementById("mySidebar").style.width = "0";
+    // this.base_right = false;
+    document.body.classList.remove("modal-visible");
+  }
+
 
 }
