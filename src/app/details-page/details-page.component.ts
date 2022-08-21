dc6a2968f346a368e2804d9766f67d6972ee5706
madhuy <madhuy@yupptv.com>
2022-08-21 20:54:01 +0530

    details page made dynamic

--- src/app/details-page/details-page.component.ts
+++ src/app/details-page/details-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute, Params } from '@angular/router';
+
 
 @Component({
   selector: 'app-details-page',
@@ -10,15 +13,37 @@ export class DetailsPageComponent implements OnInit {
   public src : any;
 public MainImagePath ='https://designmodo.com/demo/product-page/images/black.png';
 public ImagePath = 'https://designmodo.com/demo/product-page/images/red.png'
+public product_data : any;
+public param:any;
 
-  constructor() { }
+public productDetails : any;
+
+  constructor(private httpC : HttpClient, private activatedRouter : ActivatedRoute) {
+
+    
+    let url = '/assets/product_data.json'
+    this.httpC.get<any>(url).subscribe(data => {
+      this.product_data = data;
+      this.getdata(this.product_data)
+   })
+
+   }
 
   ngOnInit(): void {
+    
   }
 
   dd(){
     console.log("jhfewjh")
     localStorage.setItem("lastname", "Smith");
+  }
+  getdata(product_data: any){
+    this.param = this.activatedRouter.snapshot.paramMap.get('key');
+    for(let i=0; i < product_data.length;i++){
+      if(product_data[i].ID == this.param){
+        this.productDetails = product_data[i]
+      }
+    }
   }
   replace(){
 
